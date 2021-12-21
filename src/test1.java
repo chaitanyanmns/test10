6ea34aa49259ba1fc550a0a4bbbd1d06aa7ddd86
chaitanyanmns <47293959+chaitanyanmns@users.noreply.github.com>
2021-12-21 12:17:13 +0530

    5-feature

--- src/test1.java
+++ src/test1.java
@@ -6,6 +6,6 @@ public class test1 {
 		System.out.println("In m1 A");
 	  	//changes4
 	  	System.out.println("In m1 B1");
-	 	 System.out.println("In m1 C1");
+	 	 System.out.println("In m1 C");
 	}
 }
