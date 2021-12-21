bcf43b4ce132d67d662831862a90f668fc3e7976
chaitanyanmns <47293959+chaitanyanmns@users.noreply.github.com>
2021-12-21 13:07:16 +0530

    6-master

--- src/test1.java
+++ src/test1.java
@@ -4,7 +4,7 @@ public class test1 {
   public static void main(String[] args) {
 		System.out.println("In m1 A");
 	  	//changes4
-	  	System.out.println("In m1 B1");
-	 	 System.out.println("In m1 C1");
+	  	System.out.println("In m1 B2");
+	 	 System.out.println("In m1 C2");
 	}
 }
